fb0047b65f3ce2a306c05aa8a319aadb09c593c4
jrn011 <jrn011@bucknell.edu>
2018-04-02 19:20:16 -0400

    update

--- dbconnect.php
+++ dbconnect.php
@@ -15,7 +15,7 @@ if(!$db)
 
 $hId = $_GET['title']; //we're getting the passed hId as a paramater in the url
 
-$query = $db->prepare("SELECT name FROM movies WHERE title=:id");
+$query = $db->prepare("SELECT title FROM movies WHERE title=:id");
 $query->bindValue(':id', $hId, SQLITE3_INTEGER);
 $results = $query->execute()->fetchArray();
 echo $results['name'];
